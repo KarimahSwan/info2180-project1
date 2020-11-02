a57d6ff506abe2ecb57cbe220a5115c3d8563fbb
Karimah Swan <karimahswan@gmail.com>
2020-11-01 19:06:40 -0500

    Edited js file

--- scripts.js
+++ scripts.js
@@ -4,17 +4,26 @@ var starter=function(){
     button.addEventListener("click",function(e){
         var emailaddress= document.getElementById("email").value;
         console.log(emailaddress);
-        if (emailaddress==""){
+        if (emailaddress=="" || ValidateEmail(emailaddress)==false){
             var deconfirmationmsg="Please enter a valid email address";
             var message=document.getElementsByClassName("message")[0];
             message.textContent=deconfirmationmsg;
         }
-        else if (emailaddress!= null){
+        else if (emailaddress!= null && ValidateEmail(emailaddress)==true){
             var confirmationmsg = "Thank you your email address " +emailaddress+" has been added to our mailing list!";
             var message=document.getElementsByClassName("message")[0];
             message.textContent=confirmationmsg;
         }
         e.preventDefault();
     });  
+    function ValidateEmail(emailaddress){
+        var mailformat =/^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+        if (emailaddress.match(mailformat)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
 window.onload=starter;
